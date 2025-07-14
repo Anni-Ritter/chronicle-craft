1b58bdf2a51028ccc7b84fb7ca41763a81fda7f6
Anastasia <afon.anastasia@mail.ru>
2025-07-14 17:28:44 +0300

    update auth form

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import { FloatingInput } from './FloatingInput';
-import { Eye, EyeOff } from 'lucide-react';
+import { Button } from './ChronicleButton';
 
 export const AuthForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isSignUp, setIsSignUp] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [showPassword, setShowPassword] = useState(false)
 
     const handleAuth = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -27,10 +26,9 @@ export const AuthForm = () => {
             onSubmit={handleAuth}
             className="bg-[#223120] border border-[#c2a774] text-[#e5d9a5] font-lora px-4 py-6 rounded-2xl shadow-md space-y-6"
         >
-            <h3 className="text-xl text-center font-bold tracking-wide">
+            <h3 className="text-2xl text-center font-bold tracking-wide">
                 {isSignUp ? 'Регистрация' : 'Вход'}
             </h3>
-
 
             <div className="mb-4">
                 <FloatingInput
@@ -43,39 +41,32 @@ export const AuthForm = () => {
                 />
             </div>
 
-            <div className="mb-4 relative">
+            <div className="mb-4">
                 <FloatingInput
                     label="Пароль"
-                    type={showPassword ? 'text' : 'password'}
+                    type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                     className="w-full px-4 py-2 rounded-xl bg-[#0e1b12] text-[#f5e9c6] border border-[#c2a774]"
                 />
-                <button
-                    type="button"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                    className="absolute right-3 top-1/2 -translate-y-1/2 text-[#c2a774] border-none"
-                >
-                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                </button>
             </div>
 
             {error && (
                 <p className="text-red-600 text-sm mb-3 text-center">{error}</p>
             )}
 
-            <button
+            <Button
                 type="submit"
                 className="w-full bg-[#c2a774] hover:bg-[#e5d9a5] text-[#2D422B] py-2 rounded-xl font-semibold transition"
             >
                 {isSignUp ? 'Создать аккаунт' : 'Войти'}
-            </button>
+            </Button>
 
             <button
                 type="button"
                 onClick={() => setIsSignUp(!isSignUp)}
-                className="w-full text-sm underline hover:text-[#e5d9a5] transition border-none"
+                className="w-full text-base underline hover:text-[#e5d9a5] transition border-none"
             >
                 {isSignUp ? 'Уже есть аккаунт? Войти' : 'Нет аккаунта? Зарегистрироваться'}
             </button>
